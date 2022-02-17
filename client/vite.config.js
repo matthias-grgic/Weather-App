149ce66ded61c597fd6902d53da37d3f0c089d65
MG <matthias.grgic@gmail.com>
2022-02-17 14:34:46 +0100

    changed server routes for heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { PORT = 4000 } = process.env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/weather': `http://localhost:${PORT}`,
+    },
+  },
 })
