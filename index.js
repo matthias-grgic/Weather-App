1a08cb268b543e17692727aa6c1901c649fbd724
MG <matthias.grgic@gmail.com>
2022-02-17 13:28:23 +0100

    changed json and vercel config for heroku deployment

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 import cors from 'cors'
 import express from 'express'
 import router from './weather/index.js'
-import path from 'path'
-
-// NOT WORKING YET
-
-// const whitelist = ['http://127.0.0.1', 'http://127.0.0.1:5500']
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origim || whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-//   optionsSuccessStatus: 200,
-// }
-
-const __dirname = process.cwd()
 
 const server = express()
 server.use(cors())
@@ -30,12 +13,5 @@ server.get('/', (req, res) => res.json({ success: 'Hell World!' }))
 
 //Server Routes
 server.use('/weather', router)
-
-//Route
-server.use(express.static(path.join(__dirname, './dist')))
-
-server.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist', 'index.html'))
-})
 
 server.listen(Port, () => console.log(`Server is running on port ${Port}`))
