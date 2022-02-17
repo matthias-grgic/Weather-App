36f694ccba2cb47f2e857d51a74ffb35c33f0d9a
MG <matthias.grgic@gmail.com>
2022-02-17 14:29:41 +0100

    changed server routes for heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import router from './weather/index.js'
+import path from 'path'
 
 const server = express()
 server.use(cors())
@@ -8,10 +9,19 @@ server.use(express.json())
 
 const Port = process.env.PORT || 4000
 
+const __dirname = process.cwd()
+
 //Test Route
-server.get('/', (req, res) => res.json({ success: 'Hell World!' }))
+server.get('/test', (req, res) => res.json({ success: 'Hell World!' }))
 
 //Server Routes
 server.use('/weather', router)
+
+//For Deployment
+server.use(express.static(path.join(__dirname, './client/dist')))
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist', 'index.html'))
+})
 
 server.listen(Port, () => console.log(`Server is running on port ${Port}`))
